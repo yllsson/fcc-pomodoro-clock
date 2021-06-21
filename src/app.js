54e91c1a3cd817d6291af3af1f9ff6aba73e17a0
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2021-06-21 16:32:59 +0100

    Breaking out PomodoroApp as its own component

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PomodoroApp from './components/PomodoroApp';
 
-console.log('Hello world');
-
-class App extends React.Component {
-  render() {
-    return <h1>Pomodoro clock</h1>;
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <PomodoroApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ src/components/PomodoroApp.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PomodoroApp = () => {
+  return <h1>Pomodoro clock</h1>;
+};
+
+export default PomodoroApp;
