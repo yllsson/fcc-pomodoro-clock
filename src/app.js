717a5a7c92f36c86463619fce8224746e5db9781
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2021-06-21 16:38:49 +0100

    Adding basic development styles

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PomodoroApp from './components/PomodoroApp';
+import './styles/style.scss';
 
 ReactDOM.render(
   <React.StrictMode>
