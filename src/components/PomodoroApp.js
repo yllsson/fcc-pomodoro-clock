3841fce19257b04ce0ea328218996187c7540ce9
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-06-22 21:17:45 +0100

    Setting up part of initial structure

--- src/components/PomodoroApp.js
+++ src/components/PomodoroApp.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const PomodoroApp = () => {
-  return <h1>Pomodoro clock</h1>;
+  return (
+    <main>
+      <h1>Pomodoro clock</h1>
+      <section className='breakContainer'>
+        <h2 id='break-label'>Break Length</h2>
+        <button id='break-decrement'>-</button>
+        <div class='breakDisplay'>5</div>
+        <button id='break-increment'>+</button>
+      </section>
+
+      <section className='sessionContainer'>
+        <h2 id='session-label'>Session Length</h2>
+        <button id='session-decrement'>-</button>
+        <button id='session-increment'>+</button>
+      </section>
+    </main>
+  );
 };
 
 export default PomodoroApp;
