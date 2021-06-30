da8950dfe26bb9f858c4e84bb1b4773141f51bf4
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-06-30 22:29:07 +0100

    Initial work on timer functionality. Creating didMount to allow for first render skip.

--- src/components/PomodoroApp.js
+++ src/components/PomodoroApp.js
@@ -3,17 +3,20 @@ import Settings from './Settings';
 // import Session from './Session';
 
 const PomodoroApp = () => {
+  const [didMount, setDidMount] = useState(false);
   const [breakLength, setBreakLength] = useState(5);
   const [sessionLength, setSessionLength] = useState(25);
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(25);
+  const [seconds, setSeconds] = useState(3);
   const [sessionName, setSessionName] = useState('Session');
   // const [sessionName, setSessionName] = useState('Break');
   const [isRunning, setIsRunning] = useState(false);
   const [loop, setLoop] = useState();
 
   const startInterval = () => {
-    return setInterval(() => console.log('interval is running'), 1000);
+    return setInterval(() => {
+      setSeconds((prevState) => prevState - 1);
+    }, 1000);
   };
 
   const handleStartStop = () => {
@@ -27,6 +30,15 @@ const PomodoroApp = () => {
       clearInterval(loop);
     }
   };
+
+  useEffect(() => setDidMount(true), []);
+
+  /* this allows seconds to turn to 59 as soon as the timer kicks off, but it also makes seconds 59 on reset, so that's still an issue */
+  // useEffect(() => {
+  //   if (didMount && seconds === 0) {
+  //     setSeconds(59);
+  //   }
+  // }, [seconds]);
 
   return (
     <main>
@@ -48,7 +60,8 @@ const PomodoroApp = () => {
       <section className='container flexColumn'>
         <h2 id='timer-label'>{sessionName}</h2>
         <p id='time-left'>
-          {minutes}:{seconds}
+          {minutes < 10 ? `0${minutes}` : minutes}:
+          {seconds < 10 ? `0${seconds}` : seconds}
         </p>
 
         <div className='container flexRow'>
@@ -60,7 +73,15 @@ const PomodoroApp = () => {
           >
             {!isRunning ? 'Start' : 'Pause'}
           </button>
-          <button id='reset' onClick={() => clearInterval()}>
+          <button
+            id='reset'
+            onClick={() => {
+              setMinutes(25);
+              setSessionLength(25);
+              setBreakLength(5);
+              setSeconds(0);
+            }}
+          >
             Reset
           </button>
         </div>
