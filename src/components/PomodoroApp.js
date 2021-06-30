429fbd254a50ebb33c97da62ea7e6908885c09fe
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-06-30 22:07:20 +0100

    Started over - Finally made a functioning interval!

--- src/components/PomodoroApp.js
+++ src/components/PomodoroApp.js
@@ -1,51 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import Settings from './Settings';
-import Session from './Session';
+// import Session from './Session';
 
 const PomodoroApp = () => {
   const [breakLength, setBreakLength] = useState(5);
   const [sessionLength, setSessionLength] = useState(25);
-  const [isRunning, setIsRunning] = useState(false);
-  const [minutes, setMinutes] = useState(25);
+  const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
   const [sessionName, setSessionName] = useState('Session');
   // const [sessionName, setSessionName] = useState('Break');
+  const [isRunning, setIsRunning] = useState(false);
+  const [loop, setLoop] = useState();
 
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  let secondInterval;
-  let minuteInterval;
+  const startInterval = () => {
+    return setInterval(() => console.log('interval is running'), 1000);
+  };
 
-  // handleStartStop should start or pause the timer
   const handleStartStop = () => {
     setIsRunning((prevState) => !prevState);
-  };
 
-  const decrementSeconds = () => {
-    setSeconds((prevSeconds) => prevSeconds - 1);
-    console.log('minutes are:', minutes, 'seconds are:', seconds, isRunning);
-  };
-  const decrementMinutes = () => {
-    // setSeconds(59)
-    setMinutes((prevMinutes) => prevMinutes - 1);
-  };
-
-  // tried to make it so this useEffect triggers the interval as soon as the handleStartStop flicks the "isRunning" on or off. But this still doesn't work so need other solution.
-  useEffect(() => {
-    if (seconds === 0) {
-      setSeconds(59);
-    }
-
-    if (isRunning) {
-      console.log('clearing intervals');
-      clearInterval(secondInterval);
-      clearInterval(minuteInterval);
+    if (!isRunning) {
+      console.log(isRunning);
+      setLoop(startInterval());
     } else {
-      secondInterval = setInterval(decrementSeconds, 1000);
-      minuteInterval = setInterval(decrementMinutes, 60000);
+      console.log('cleaning');
+      clearInterval(loop);
     }
-  }, [isRunning]);
+  };
 
   return (
     <main>
@@ -65,15 +46,24 @@ const PomodoroApp = () => {
       </div>
 
       <section className='container flexColumn'>
-        <Session
-          name={sessionName}
-          seconds={seconds}
-          handleStartStop={handleStartStop}
-          isRunning={isRunning}
-          minutes={minutes}
-          secondInterval={secondInterval}
-          minuteInterval={minuteInterval}
-        />
+        <h2 id='timer-label'>{sessionName}</h2>
+        <p id='time-left'>
+          {minutes}:{seconds}
+        </p>
+
+        <div className='container flexRow'>
+          <button
+            id='start_stop'
+            onClick={() => {
+              handleStartStop();
+            }}
+          >
+            {!isRunning ? 'Start' : 'Pause'}
+          </button>
+          <button id='reset' onClick={() => clearInterval()}>
+            Reset
+          </button>
+        </div>
       </section>
     </main>
   );
--- src/components/Session.js
+++ src/components/Session.js
@@ -1,42 +1,9 @@
-import React from 'react';
+// import React from 'react';
 
-const Session = ({
-  name,
-  minutes,
-  seconds,
-  handleStartStop,
-  isRunning,
-  secondInterval,
-  minuteInterval
-}) => {
-  return (
-    <div id='time-left' className='container flexColumn'>
-      <h2 id='timer-label'>{name}</h2>
-      <p>
-        {minutes}:{seconds}
-      </p>
+// const Session = ({ name, minutes, seconds, handleStartStop, isRunning }) => {
+//   return (
 
-      <div className='container flexRow'>
-        <button
-          id='start_stop'
-          onClick={() => {
-            handleStartStop(minutes);
-          }}
-        >
-          {!isRunning ? 'Start' : 'Pause'}
-        </button>
-        <button
-          id='reset'
-          onClick={() => {
-            clearInterval(secondInterval);
-            clearInterval(minuteInterval);
-          }}
-        >
-          Reset
-        </button>
-      </div>
-    </div>
-  );
-};
+//   );
+// };
 
-export default Session;
+// export default Session;
