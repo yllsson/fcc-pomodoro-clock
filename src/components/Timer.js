f1aed297bc35056d075a4624ee6be766e6f9ee7b
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2021-07-12 12:15:44 +0100

    Delete Timer.js

--- src/components/Timer.js
+++
@@ -1,9 +0,0 @@
-// import React, { useState } from 'react';
-
-// const Timer = ({ minutes, seconds, setMinutes, setSeconds }) => {
-//   return (
-//     <div id='time-left'>
-//       {minutes}:{seconds}
-//     </div>
-//   );
-// };
